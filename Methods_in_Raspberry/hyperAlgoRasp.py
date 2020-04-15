4c782cab40fdb84014e32ca76c4164e4c9647077
tiagotrocoli <tiago1trocoli@gmail.com>
2020-04-15 15:39:39 -0300

    Energy tests

--- Methods_in_Raspberry/hyperAlgoRasp.py
+++ Methods_in_Raspberry/hyperAlgoRasp.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb  4 13:45:22 2020
+Created on Wed Apr 15 09:17:32 2020
 
-@author: mister-c
+@author: tiago
 """
 
 import time
@@ -58,15 +58,6 @@ def getTestData(doc):
         l_rssi[3].append(locale.atof(c4.value))
         l_rssi[4].append(locale.atof(c5.value))
         l_rssi[5].append(locale.atof(c6.value))
-
-def lognomal(rssi):
-
-    d.append(findDistance(-23.1615, 4.3478, rssi[0]) )
-    d.append(findDistance(-2.4133, 5.7772, rssi[1]))
-    d.append(findDistance(52.6093, 14.6044, rssi[2]))
-    d.append(findDistance(0.2455, 7.4626, rssi[3]))
-    d.append(findDistance(38.7733, 11.7859,rssi[4]))
-    d.append(findDistance(49.1173, 13.8565, rssi[5]))
     
 def polyRegression(rssi):    
     
@@ -76,35 +67,6 @@ def polyRegression(rssi):
     d.append(polynomial(rssi[3],3.42350116e+01,1.80886737e+00,2.01219351e-02,-2.59560664e-04,-3.61977580e-06))
     d.append(polynomial(rssi[4],5.41055459e+02,4.39397008e+01,1.31063670e+00,1.69009265e-02,7.98114859e-05))
     d.append(polynomial(rssi[5],-2.30666363e+01,-3.59322884e+00,-1.54346312e-01,-2.70287489e-03,-1.65517534e-05))
-
-def hyperbolicAlgorithm(rssi):
-    
-    polyRegression(rssi)
-    
-    A = np.matrix([[ 2*(x[5] - x[0]), 2*(y[5] - y[0]) ], 
-                   [ 2*(x[5] - x[1]), 2*(y[5] - y[1]) ],
-                   [ 2*(x[5] - x[3]), 2*(y[5] - y[3]) ],
-                   [ 2*(x[5] - x[4]), 2*(y[5] - y[4]) ]
-                   #[ 2*(x[4] - x[4]), 2*(y[5] - y[4]) ]
-                  ], dtype=np.float64)
-    b= np.matrix([
-                [d[0]**2 - d[5]**2 - x[0]**2 - y[0]**2 + x[5]**2 + y[5]**2],
-                [d[1]**2 - d[5]**2 - x[1]**2 - y[1]**2 + x[5]**2 + y[5]**2],
-                [d[3]**2 - d[5]**2 - x[3]**2 - y[3]**2 + x[5]**2 + y[5]**2],
-                [d[4]**2 - d[5]**2 - x[4]**2 - y[4]**2 + x[5]**2 + y[5]**2],
-                #[d[4]**2 - d[4]**2 - x[4]**2 - y[4]**2 + x[4]**2 + y[4]**2]
-                ], dtype=np.float64)
-    
-    AT  = A.transpose()
-    AAT = AT.dot(A)
-    d.clear()
-    try:
-        var = (inv(AAT)*AT*b).tolist()
-        return [var[0][0], var[1][0]]
-    except:
-        print("Could not solve since matrix has no inverse.")
-        return -1
-        #3.4760209715908
 
 def hyperbolicAlgorithm1(rssi):
     
@@ -142,26 +104,22 @@ def main():
     
     getTestData('testPoints.xlsx')
     n = len(position)
-    avg = 0
 
-    for i in range(n):
-        rssi = [l_rssi[0][i],l_rssi[1][i],l_rssi[2][i],l_rssi[3][i],l_rssi[4][i],l_rssi[5][i]]
-        #calculate duration
-        start = time.time()
-        res = hyperbolicAlgorithm1(rssi)
+    count = 0
+    log = open("Log.txt", "w+")
+    minutes = 60
+    start = time.time()
+    while(True):
+        count = count + 1
+        log = open("Log.txt", "a")
+        for i in range(n):
+            rssi = [l_rssi[0][i],l_rssi[1][i],l_rssi[2][i],l_rssi[3][i],l_rssi[4][i],l_rssi[5][i]]
+            hyperbolicAlgorithm1(rssi)
         duration = time.time() - start
-        # calculate precision
-        error = math.sqrt( (position[i][0] - res[0])**2 + (position[i][1] - res[1])**2 )
-        # put them together
-        data = [round(position[i][0],8), round(position[i][1],8)]
-        data.extend([round(res[0],8), round(res[1], 8)])
-        data.append(round(duration,8))
-        data.append(round(error,8))
-        print (','.join(str(x) for x in data))
-        avg = avg + error
-        # store in xlsx
-        #storeData(data,"HyperbolicAlgo")
+        if duration - minutes > 0:
+            log.write(str(minutes/60) + " " + str(count) + "\n")
+            log.close()
+            minutes = minutes + 60
         
-    print(avg/18.0)
 if __name__== "__main__":
         main()
